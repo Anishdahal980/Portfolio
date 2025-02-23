62de54ba1e3e627bcd01ef6f384ae5282978e001
Anish980 <anishdahal190@gmail.com>
2025-02-23 13:46:16 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+$(".menu-toggle-btn").click(function () {
+  $(this).toggleClass("fa-times");
+  $(".navigation-menu").toggleClass("active");
+});
+
+function checkValidations() {
+  let letters = /^[a-zA-Z\s]*$/;
+  const name = nameEl.value.trim();
+  const email = emailEl.value.trim();
+  const companyName = companyNameEl.value.trim();
+  const message = messageEl.value.trim();
+  if (name === "") {
+    document.querySelector(".name-error").classList.add("error");
+    document.querySelector(".name-error").innerText =
+      "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+    }
+  }
+  if (email === "") {
+    document.querySelector(".name-error").classList.add("error");
+    document.querySelector(".name-error").innerText =
+      "Please fill out this field!";
+  } else {
+    if (!letters.test(name)) {
+      document.querySelector(".name-error").classList.add("error");
+      document.querySelector(".name-error").innerText =
+        "Please enter only characters!";
+    } else {
+    }
+  }
+}
+
+function reset() {
+  nameEl = "";
+  emailEl = "";
+  companyNameEl = "";
+  messageEl = "";
+  document.querySelector(".name-error").innerText = "";
+}
